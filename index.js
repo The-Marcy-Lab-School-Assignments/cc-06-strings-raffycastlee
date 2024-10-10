c2b44258babab568dba24d4addccc2dd50dcd8c5
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-10 10:51:58 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 //write your code here
+
+const reverseString = (str) => {
+  res = "";
+
+  for (let i = str.length-1; i >= 0; i--) {
+    res += str[i];
+  }
+
+  return res
+}
+
+// console.log(reverseString("hello"))  //"olleh"
+// console.log(reverseString("oo"))     //"oo"
+// console.log(reverseString(""))       //""
+
+const reverseZigZagString = (str) => {
+  res = "";
+
+  let small = true;
+  for (let i = str.length-1; i >= 0; i--) {
+    if (small) {
+      res += str[i].toLowerCase();
+      small = false;
+    } else {
+      res += str[i].toUpperCase();
+      small = true;
+    }
+  }
+
+  return res;
+}
+
+// console.log(reverseZigZagString("hello")) //"oOlLeH"
+// console.log(reverseZigZagString("Fellows"))    //"sWoLlEf"
+// console.log(reverseZigZagString("Code Challenge"))  //"eGnElLaHc EdOc"
